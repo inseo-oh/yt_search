2262db79ccdaa29aef02f5a6005ed8d17db8d8e8
Inseo Oh <dhdlstjtr@gmail.com>
2023-10-26 17:15:22 +0900

    Fix search box's search button URL

--- src/component/SearchBox.js
+++ src/component/SearchBox.js
@@ -4,7 +4,7 @@ import { BsSearch } from 'react-icons/bs';
 
 export default function SearchBox({ light }) {
   const [searchKeyword, setSearchKeyword] = useState("");
-  const searchUrl = `/search/${encodeURIComponent(searchKeyword)}`;
+  const searchUrl = `#/search/${encodeURIComponent(searchKeyword)}`;
   return <div className={`${style.Container} ${light ? style.Light : ''}`}>
     <input className={style.SearchQueryInput} value={searchKeyword} onChange={e => setSearchKeyword(e.target.value)} type='text' placeholder='검색어를 입력하세요' />
     <a className={style.SearchButton} href={searchUrl}><BsSearch /></a>
